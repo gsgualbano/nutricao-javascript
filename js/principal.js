f9f962687b28d09296caa828b83d4dfae47f5513
Gustavo <Gustavo Scherer>
2018-02-16 01:51:15 -0300

    Modificado o calculo do imc para se tornar generico

--- js/principal.js
+++ js/principal.js
@@ -1,20 +1,44 @@
-console.log("Fui carregado de um arquivo externo");
-
 var titulo = document.querySelector(".titulo");
-var paciente = document.querySelector("#primeiro-paciente");
-
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
-var tdGordura = paciente.querySelector(".info-gordura");
-var gordura = tdGordura.textContent;
-
-var tdImc = paciente.querySelector(".info-imc");
-var imc = parseFloat(peso)/(parseFloat(altura) * parseFloat(altura));
-
-
-tdImc.textContent = imc;
 titulo.textContent =  "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".pacientes");
+
+for(var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    var imc = peso/(altura *altura);
+    
+    var pesoEhValido = true;
+    var alturaEhValido = true;
+
+    if(peso <= 0  ||  peso >= 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido";
+        //paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura <= 0 || altura >= 3.0) {
+        console.log("Altura invalida");
+        alturaEhValido = false;
+        tdImc.textContent = "Altura Invalido";
+        //paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValido) {
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
+
+
+
+
